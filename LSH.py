41fbe8c7c0d81ee86bb2141bd818efbfc8de6d7e
ali qasemi <qasemi4245qq@gmail.com>
2021-04-30 02:40:48 +0430

    some edit in struct

--- LSH.py
+++ LSH.py
@@ -4,12 +4,18 @@ import numpy as np
 
 
 class LSH:
+    def __init__(self, num_permutation, shingles, path):
+        self.num_permutation = num_permutation
+        self.shingles = shingles
+        self.path = path
+        self.signatures = np.full((self.num_permutation, len(self.document_sort)), "******************")
+
     document_sort = []
     matrix = set()
 
-    def __init__(self, num_permutation, shingles):
-        self.num_permutation = num_permutation
-        self.shingles = shingles
+    def handle(self):
+        self.buildMatrix(self.path)
+        self.buildSignatures()
 
     def buildMatrix(self, path):
         shingle_array = {}
@@ -29,12 +35,13 @@ class LSH:
             shingle_array.update({shingle: exist_array})
 
         print(self.document_sort)
+        self.signatures = np.full((self.num_permutation, len(self.document_sort)), "******************")
         self.matrix = shingle_array
 
         return shingle_array
 
     def buildSignatures(self):
-        signatures = np.full((self.num_permutation, len(self.document_sort)), "******************")
+
         permuteMatrix = self.matrix
         for permute in range(self.num_permutation):
             print(permuteMatrix)
@@ -43,13 +50,13 @@ class LSH:
                     if value[x] == 1:
                         position_row = permute
                         position_col = x
-                        signatures[position_row][position_col] = item
+                        self.signatures[position_row][position_col] = item
                         position = [int(len(self.document_sort) * position_row + position_col)]
-                        np.put(signatures, position, item)
+                        np.put(self.signatures, position, item)
                         break
 
             lists = list(permuteMatrix.items())
             random.shuffle(lists)
             permuteMatrix = dict(lists)
 
-        print(signatures)
+        print(self.signatures)
--- main.py
+++ main.py
@@ -4,20 +4,5 @@ from Shingle import Shingle
 path = "./DataSet"
 shingle = Shingle('word')
 
-lsh = LSH(128, shingle.searchFile(path))
-lsh.buildMatrix(path)
-
-lsh.buildSignatures()
-
-
-# arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# arr[1][0] = 5
-# print(arr)
-
-# arr = {}
-# arr.update({"d": [1, 1, 0 , 1]})
-# arr.update({"s": [1, 1, 0 , 1]})
-# arr.update({"s": [1, 1, 0 , 1]})
-#
-# for (key, value) in arr.items():
-#     print(key, value)
+lsh = LSH(128, shingle.searchFile(path), path)
+lsh.handle()
